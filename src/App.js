ae2ba991ea86aecdba8a2c2d2f7ca03c78152228
Roy Lopez <roylopezdev@gmail.com>
2023-03-09 14:33:44 +0000

    useReducer boilerplate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import { useState, useReducer } from "react";
 import "./styles.css";
 
+const reducer = (state, action) => {
+  return {count: state.count + 1};
+}
+
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {count: 0});
+  const [count, setCount] = useState(0);
 
   function increment() {
-setCount(prevCount => prevCount + 1)
+    setCount((prevCount) => prevCount + 1);
   }
 
   function decrement() {
-setCount(prevCount => prevCount - 1)
+    setCount((prevCount) => prevCount - 1);
   }
 
   return (
     <div className="App">
       <h1>Counter</h1>
+      <h2>{count}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
