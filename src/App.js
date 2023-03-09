bc3fd5a78af6ef4f7cb18d65a6ad49c5bb187e4d
Roy Lopez <roylopezdev@gmail.com>
2023-03-09 14:23:36 +0000

    basic app with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+setCount(prevCount => prevCount + 1)
+  }
+
+  function decrement() {
+setCount(prevCount => prevCount - 1)
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
